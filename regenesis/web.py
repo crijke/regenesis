eeb0d1e1c3afd9edc6fd74636919542199a45c9a
Christian Rijke <hello@christianrijke.de>
2019-10-16 22:07:38 +0200

    Fix imports, add slugify dependency

--- regenesis/web.py
+++ regenesis/web.py
@@ -3,7 +3,7 @@ from flask import render_template, Response, request
 from flask import session, redirect, flash, Markup, url_for
 
 from regenesis.core import app
-from regenesis.util import slugify as _slugify
+from slugify import slugify
 from regenesis.views.util import dimension_type_text as _dimension_type_text
 from regenesis.views.dimension import blueprint as dimension_blueprint
 from regenesis.views.statistic import blueprint as statistic_blueprint
@@ -36,13 +36,13 @@ def text_filter_wrapped(s):
 
 @app.template_filter()
 def slugify(text):
-    return _slugify(text)
+    return slugify(text)
 
 
 @app.context_processor
 def set_template_globals():
     return {
-        'slugify': _slugify,
+        'slugify': slugify,
         'API': app.config.get('API_ENDPOINT')
     }
 
